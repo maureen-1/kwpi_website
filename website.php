387a9808a2eae0f546008dfc2c10f1296e9a1111
Maureen-1 <maureenanthony607@gmail.com>
2023-07-26 13:14:11 +0300

    init

---
+++ website.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VOluntary Website</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>   
+</body>
+  <header>
+    <h1>Kenya Women Prisoners Initiative</h1>
+    <nav>
+      <ul class="menu">
+        <li>
+          Our Work
+          <ul class="sub-menu">
+            <li>Education and awareness</li>
+            <li>Donate & Fundraising</li>
+            <li>Patnering with NGO'S &organizations</li>
+            <li>Stories and  testimonials</li>
+            <li>Post-realease support</li>
+            <li>Health and mental health support</li>
+          </ul>
+        </li>
+        <li>
+          About us
+          <ul class="sub-menu">
+            <li>Our Vision</li>
+            <li>Contact</li>
+          </ul>
+        </li>
+        <li>
+          Support us
+          <ul class="sub-menu">
+            <li>Donate</li>
+            <li>Partner</li>
+            <li>Volunteer</li>
+          </ul>
+        </li>
+        
+
+        
+        </li>
+      </ul>
+     </nav>
+
+    </header>
+  </body>
+</html>
+
+
+  
